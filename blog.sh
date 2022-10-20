2d117d2d1d49e65ecea96a58eb971a8c05a5366d
dimitri <dimitri.dd@outlook.fr>
2022-10-20 08:54:40 +0200

    Generate html file

--- blog.sh
+++ blog.sh
@@ -8,7 +8,21 @@ then
 while read line; do echo $line; done < help.txt
 fi 
 
+touch -a < test.html
 
+cat > test.html << EOF
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>New Page</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+  </body>
+</html>
+
+EOF
 
 
 
